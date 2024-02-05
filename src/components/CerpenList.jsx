fa0bfe8c39d36df0d7695b178263eb0b26296c23
ronalyulyanto <ronal.belajar.mengajar@gmail.com>
2024-02-05 10:45:53 +0000

    feat: responsive lg

--- src/components/CerpenList.jsx
+++ src/components/CerpenList.jsx
@@ -5,7 +5,7 @@ import { Cerpen } from "./Cerpen";
 export const CerpenList = ({dataCerpen}) => {
 
     return (
-    <main className="card grid grid-cols-3 gap-2">
+    <main className="card lg:grid lg:grid-cols-3 gap-2">
     {/* <main className="card m-4 w-80 shadow "> */}
       {dataCerpen.map((item, index)=> {
           return (
